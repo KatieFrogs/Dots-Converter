db54f3b8d607dc28515dc97c4cc681174d06d1d6
Ruan Beukes <ruan.coder@gmail.com>
2020-05-20 21:25:21 +0800

    move imageTo6Dots to its own file

--- retro.js
+++ retro.js
@@ -1,28 +1,10 @@
+import { imageTo6Dots } from "./imageTo6Dots.js";
+
 (function extendDotsObject() {
   console.log("extendDotsObject");
 
   dots = dots || {};
-
-  dots.imageTo6Dots = function () {
-    if (dots.file) {
-      dots.file();
-      // hack - wait 1 sec for the dot.file() to finish, then execute the actual imageTo6Dots code
-      setTimeout(() => {
-        var brailleText = di("txt").value;
-        var newImageWith6Dots = "";
-        for (i = 0; i < brailleText.length; i++) {
-          // clear dot 7 and 8, only want dots 1 to 6
-          newImageWith6Dots += String.fromCharCode(
-            brailleText[i].charCodeAt() & 0xff3f
-          );
-        }
-        // console.log(newImage);
-        di("txt").value = newImageWith6Dots;
-      }, 1000);
-    } else {
-      console.error("Failed - Could not find dots.file()");
-    }
-  };
+  dots.imageTo6Dots = imageTo6Dots;
 })();
 
 (function initInputValues() {
